45a9da56af7f2b6b5eda0e738906187124357b34
zakariya mohamed <zakariyakassim@live.com>
2018-08-29 11:26:38 +0100

    initial commit

---
+++ src/main/java/com/qa/HelloWord/HelloWordApplication.java
@@ -0,0 +1,12 @@
+package com.qa.HelloWord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWordApplication.class, args);
+	}
+}
